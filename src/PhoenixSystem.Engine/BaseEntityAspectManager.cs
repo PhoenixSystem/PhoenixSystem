df6a98a16add452e79a65237fe226833e2896846
Jonathan Mast <Jonathan Mast>
2016-02-10 21:18:44 -0500

    interim don't be mad at me for being bad at git

---
+++ src/PhoenixSystem.Engine/BaseEntityAspectManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSystem.Engine
+{
+    public abstract class BaseEntityAspectManager<AspectFamily>
+    {
+        public BaseGameManager GameManager
+        {
+            get;
+            set;
+        }
+
+        
+
+    }
+}
---
+++ src/PhoenixSystem.Engine/IEntityAspectMatchingFamily.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSystem.Engine
+{
+    public interface IEntityAspectMatchingFamily
+    {
+        void Init();
+        IEnumerable<BaseAspect> GetAspectList();
+        IEnumerable<BaseAspect> GetEntireAspectList();
+        void NewEntity(Entity e);
+        void RemoveEntity(Entity e);
+        //void ComponentAddedToEntity<ComponentType>(Entity e) : 
+
+        //ComponentAddedToEntity: componentAddedToEntity,
+        //        ComponentRemovedFromEntity: componentRemovedFromEntity,
+        //        CleanUp: cleanUp
+
+    }
+}
