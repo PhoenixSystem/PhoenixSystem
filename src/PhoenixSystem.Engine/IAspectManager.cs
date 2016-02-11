6d4bae83ad88d4404ae7569026218dbad745bc51
Jonathan Mast <jonathanpmast@hotmail.com>
2016-02-11 00:04:01 -0500

    IAspectManager interface inserted

---
+++ src/PhoenixSystem.Engine/IAspectManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoenixSystem.Engine {
+    public interface IAspectManager<AspectType> where AspectType : BaseAspect, new() 
+    {
+        void ClearCache();
+        AspectType Get(Entity e);
+        IEnumerable<AspectType> ActiveAspects { get; }
+        IEnumerable<AspectType> ChannelAspects { get; }
+        int AvailableAspectCount { get; }
+    }
+}
