c8cb5c190b4d317bacf7f32862d8247060c3ef8b
Jonathan Mast <jonathanpmast@hotmail.com>
2016-02-14 01:03:43 -0500

    initialized some of the testing infrastructure

---
+++ src/PhoenixSystem.Engine.Tests/Classes/LabelAspect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PhoenixSystem.Engine.Attributes;
+namespace PhoenixSystem.Engine.Tests.Classes
+{
+    [AssociatedComponents(new Type[] { typeof(StringComponent), typeof(XYComponent) })]
+    class LabelAspect : BaseAspect
+    {
+        public LabelAspect() : base()
+        {
+
+        }
+        
+    }
+}
---
+++ src/PhoenixSystem.Engine.Tests/Classes/XYComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSystem.Engine.Tests.Classes
+{
+    class XYComponent : BaseComponent
+    {
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+        public override IComponent Clone()
+        {
+            return new XYComponent()
+            {
+                X = this.X,
+                Y = this.Y
+            };
+        }
+
+        public override void Reset()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
