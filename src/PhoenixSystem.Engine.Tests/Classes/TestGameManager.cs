79388cff4036ebcf30b029fd24d36e2153fbab4a
Jonathan Mast <jonathanpmast@hotmail.com>
2016-02-14 02:56:37 -0500

    adds more test crap

---
+++ src/PhoenixSystem.Engine.Tests/Classes/TestGameManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSystem.Engine.Tests.Classes
+{
+    class TestGameManager : BaseGameManager
+    {
+        protected TestGameManager(IEntityAspectManager entityAspectManager) : base(entityAspectManager)
+        {
+
+        }
+    }
+}
