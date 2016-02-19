71020fa3268010c82efc9c6e20b632f51698f132
Jonathan Mast <jonathanpmast@hotmail.com>
2016-02-19 12:13:19 -0500

    adds channelmanager tests

---
+++ src/PhoenixSystem.Engine.Tests/ChannelManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using PhoenixSystem.Engine;
+public class ChannelManagerTests
+{
+    public ChannelManagerTests() { }
+
+    [Fact]
+    public void Should_Notify_When_Channel_Changes()
+    {
+        IChannelManager cm = new BasicChannelManager();
+        bool notify = false;
+        cm.ChannelChanged += (s, e) => notify = true;
+        cm.Channel = "different";
+        Assert.True(notify);
+    }
+}
